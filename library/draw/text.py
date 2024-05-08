fe9d78f167bbb7e6eb7461489e28b7633a0d7fc3
Vincent Glendenning <notmutater@gmail.com>
2024-05-08 18:48:34 -0500

    wip text bug

--- library/draw/text.py
+++ library/draw/text.py
@@ -4,3 +4,6 @@ from library.draw.draw import Draw
 class Text(Draw):
     def __init__(self, dest: Coordinate, size: Coordinate):
         super().__init__(dest, size)
+    
+    def draw(self, surface: pygame.Surface):
+        super().draw(surface)
